ff0636375436a1a27f75325f068502e6bceb9caf
hertelukas <hertelukas@users.noreply.github.com>
2021-03-01 16:33:21 +0100

    Using UTF-8

--- Jubilant Server/Program.cs
+++ Jubilant Server/Program.cs
@@ -94,7 +94,7 @@ namespace Jubilant_Server
             if(bytesRead > 0)
             {
                 //There might be more data, so store the data received so far
-                state.sb.Append(Encoding.ASCII.GetString(
+                state.sb.Append(Encoding.UTF8.GetString(
                     state.buffer, 0, bytesRead));
 
                 //Check for end-of-file tag. If not there, read more data
@@ -117,7 +117,7 @@ namespace Jubilant_Server
         private static void Send(Socket handler, String data)
         {
             //Convert the string data to byte data using ASCII encoding
-            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            byte[] byteData = Encoding.UTF8.GetBytes(data);
 
             //Begin sending the data to the remote device
             handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
