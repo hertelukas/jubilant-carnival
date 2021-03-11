74b67b027ba98b16df4bf62736ae28aa9bd54058
hertelukas <hertelukas@users.noreply.github.com>
2021-03-12 00:57:36 +0100

    Removed packages from .gitignore

---
+++ Jubilant Server/Packages/UsernameTakenPackage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jubilant_Server.Packages
+{
+    class UsernameTakenPackage : Package
+    {
+
+        public UsernameTakenPackage(int id, Socket socket) : base("", socket)
+        {
+            playerId = id;
+            packageId = PackageType.UsernameTaken;
+        }
+    }
+}
---
+++ Jubilant Server/Packages/WelcomeReceivedPackage.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Net.Sockets;
+
+namespace Jubilant_Server.Packages
+{
+    class WelcomeReceivedPackage : Package
+    {
+        public WelcomeReceivedPackage(int id, Socket socket) : base("", socket)
+        {
+            playerId = id;
+            packageId = PackageType.WelcomeReceived;
+        }
+
+        public override string GetContent()
+        {
+            return "Welcome received.";
+        }
+    }
+}
